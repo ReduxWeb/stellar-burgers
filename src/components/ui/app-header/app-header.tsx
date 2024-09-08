5e90bd4ff753d0580f71fb52019825a73c4c2943
Artur Semenov <celweb98@gmail.com>
2024-09-08 22:12:00 +0300

    profile

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -14,19 +14,28 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            clsx(styles.link, { [styles.link_active]: isActive })
+          }
+        >
           <BurgerIcon type={'primary'} />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
+        </NavLink>
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            clsx(styles.link, { [styles.link_active]: isActive })
+          }
+        >
           <ListIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
+        </NavLink>
       </div>
       <div className={styles.logo}>
         <Logo className='' />
       </div>
-
       <div className={styles.link_position_last}>
         <NavLink
           to='/profile'
